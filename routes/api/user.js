587e89eb67561322b8cdfb2065c97de1021cce11
Ermiyas <68846502+Yiskah27@users.noreply.github.com>
2022-12-28 07:51:35 +0300

    user route and user schema done

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const route = express.Router();
-
-route.post('/', (req, res) => {
-    res.send(req.body);
+const { check, validationResult } = require('express-validator');
+route.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('Email', "Email is required").not().isEmpty(),
+    check('Email', 'Invalid Email').isEmail(),
+    check('password', 'minimum lenght is 6').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    } else {
+        res.send(req.body);
+    }
 })
 route.get('/', (req, res) => {
     console.log("user route");
