674d25a4182124a91f96fb8bcf2699ff11d64d8d
Ermiyas <68846502+Yiskah27@users.noreply.github.com>
2022-12-30 09:33:08 +0300

    30122022

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,5 +4,7 @@ const route = express.Router();
 route.get('/', (req, res)=>{
     console.log("auth route");
 })
-
+route.post('/autho', (req, res)=>{
+    console.log('authos post');
+})
 module.exports = route;
