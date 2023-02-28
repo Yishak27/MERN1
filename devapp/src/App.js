0130ebd4801ff18558773946ee86383885dee271
Ermiyas <68846502+Yiskah27@users.noreply.github.com>
2023-02-28 17:53:09 +0300

    able to navigate through the menus

--- devapp/src/App.js
+++ devapp/src/App.js
@@ -1,13 +1,21 @@
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/layout/auth/Login';
+import Register from './components/layout/auth/Register';
+
 function App() {
   return (
-    <Fragment>
+    <>
       <Navbar />
-      <Landing/>
-    </Fragment>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
