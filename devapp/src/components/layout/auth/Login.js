0d642a8275c5eaa950190dae5372c7b24a9dfe8d
Ermiyas <68846502+Yiskah27@users.noreply.github.com>
2023-03-06 18:00:33 +0300

    login success

--- devapp/src/components/layout/auth/Login.js
+++ devapp/src/components/layout/auth/Login.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const Login = () => {
+  
+  const [fetchData, setFetchData] = useState({
+    email: "",
+    password: ""
+  });
+  const { email, password } = fetchData;
+
+  const onChange = e => setFetchData({ ...fetchData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    // asign the fetch data
+    const newUser = { email, password };
+    try {
+      //creating config
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+        }
+      };
+      const body = JSON.stringify(newUser);
+      const res = await axios.get('http://localhost:5000/API/user/', body, config);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
   return (
     <div>
       <section className="container">
@@ -10,13 +41,15 @@ const Login = () => {
       </div>
       <h1 className="large text-primary">Sign In</h1>
       <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
-      <form className="form" action="dashboard.html">
+        <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <input
             type="email"
             placeholder="Email Address"
             name="email"
             required
+            value={email}
+            onChange={e=>onChange(e)}
           />
         </div>
         <div className="form-group">
@@ -24,6 +57,8 @@ const Login = () => {
             type="password"
             placeholder="Password"
             name="password"
+            value={password}
+            onChange={e=>onChange(e)}
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Login" />
